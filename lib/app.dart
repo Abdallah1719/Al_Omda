96727999c256321324d413258e744d5aca479924
abdallah <abdallahtolba17@gmail.com>
2025-05-15 14:00:37 +0300

    add  app structure

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AlOmda extends StatelessWidget {
+  const AlOmda({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
