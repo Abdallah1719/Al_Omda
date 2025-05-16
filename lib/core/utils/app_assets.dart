3afddf02bf75efdd0e49b67eef3c79c4a929f171
abdallah <abdallahtolba17@gmail.com>
2025-05-16 14:38:41 +0300

    add images asstes

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesEgypt
+  /// asstes/images/egypt.png
+  static const String imagesEgypt = "asstes/images/egypt.png";
+
+  /// Assets for imagesFacebook
+  /// asstes/images/facebook.png
+  static const String imagesFacebook = "asstes/images/facebook.png";
+
+  /// Assets for imagesHeart
+  /// asstes/images/heart.svg
+  static const String imagesHeart = "asstes/images/heart.svg";
+
+  /// Assets for imagesLang
+  /// asstes/images/lang.png
+  static const String imagesLang = "asstes/images/lang.png";
+
+  /// Assets for imagesOmdaLogo
+  /// asstes/images/omda_logo.png
+  static const String imagesOmdaLogo = "asstes/images/omda_logo.png";
+
+  /// Assets for imagesOnboarding1
+  /// asstes/images/onboarding1.png
+  static const String imagesOnboarding1 = "asstes/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// asstes/images/onboarding2.png
+  static const String imagesOnboarding2 = "asstes/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// asstes/images/onboarding3.png
+  static const String imagesOnboarding3 = "asstes/images/onboarding3.png";
+
+  /// Assets for imagesSplashLogo
+  /// asstes/images/splash_logo.png
+  static const String imagesSplashLogo = "asstes/images/splash_logo.png";
+
+  /// Assets for imagesWhatsapp
+  /// asstes/images/whatsapp.png
+  static const String imagesWhatsapp = "asstes/images/whatsapp.png";
+}
+
