479c3740ba897e880697063456d09f3151d4a9b3
abdallah <abdallahtolba17@gmail.com>
2025-06-03 23:20:38 +0300

    refactor home repo

--- lib/features/home/data/repository/home_repository.dart
+++ lib/features/home/data/repository/home_repository.dart
@@ -14,29 +14,27 @@ class HomeRepository implements BaseHomeRepository {
     try {
       final response = await api.get(ApiConstances.homeSliderPath);
 
-      if (response is Map<String, dynamic> && response.containsKey('data')) {
-        final List<dynamic> dataList = response['data'] as List;
+      // if (response is Map<String, dynamic> && response.containsKey('data')) {
+      final List<dynamic> dataList = response['data'] as List;
 
-        final List<HomeSlidersModel> homeSliders =
-            dataList
-                .map(
-                  (e) => HomeSlidersModel.fromJson(e as Map<String, dynamic>),
-                )
-                .toList();
+      final List<HomeSlidersModel> homeSliders =
+          dataList
+              .map((e) => HomeSlidersModel.fromJson(e as Map<String, dynamic>))
+              .toList();
 
-        return Right(homeSliders);
-      } else if (response is List) {
-        final List<HomeSlidersModel> homeSliders =
-            response
-                .map(
-                  (e) => HomeSlidersModel.fromJson(e as Map<String, dynamic>),
-                )
-                .toList();
+      return Right(homeSliders);
+      // } else if (response is List) {
+      //   final List<HomeSlidersModel> homeSliders =
+      //       response
+      //           .map(
+      //             (e) => HomeSlidersModel.fromJson(e as Map<String, dynamic>),
+      //           )
+      //           .toList();
 
-        return Right(homeSliders);
-      } else {
-        return Left("Invalid response format");
-      }
+      //   return Right(homeSliders);
+      // } else {
+      //   return Left("Invalid response format");
+      // }
     } on ServerException catch (e) {
       return Left(e.errorModel.errorMessage);
     }
@@ -47,29 +45,29 @@ class HomeRepository implements BaseHomeRepository {
     try {
       final response = await api.get(ApiConstances.categoriesPath);
 
-      if (response is Map<String, dynamic>) {
-        final data = response['data'] as List?;
-        if (data != null && data.isNotEmpty) {
-          final List<HomeCategoriesModel> categories =
-              data
-                  .map(
-                    (item) => HomeCategoriesModel.fromJson(
-                      item as Map<String, dynamic>,
-                    ),
-                  )
-                  .toList();
+      // if (response is Map<String, dynamic>) {
+      final data = response['data'] as List;
+      // if (data != null && data.isNotEmpty) {
+      final List<HomeCategoriesModel> categories =
+          data
+              .map(
+                (item) =>
+                    HomeCategoriesModel.fromJson(item as Map<String, dynamic>),
+              )
+              .toList();
 
-          return Right(categories);
-        } else {
-          return Left('No categories found in the response.');
-        }
-      } else {
-        return Left('Unexpected response format');
-      }
+      return Right(categories);
+      //   } else {
+      //     return Left('No categories found in the response.');
+      //   }
+      // } else {
+      //   return Left('Unexpected response format');
+      // }
     } on ServerException catch (e) {
       return Left(e.errorModel.errorMessage);
-    } catch (e) {
-      return Left('An unexpected error occurred: $e');
     }
+    // catch (e) {
+    //   return Left('An unexpected error occurred: $e');
+    // }
   }
 }
