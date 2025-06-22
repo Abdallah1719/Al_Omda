46614d8de87067939a63de53942a650b2d1aad89
abdallah <abdallahtolba17@gmail.com>
2025-06-22 10:16:49 +0300

    add makeOrder method in order repo

--- lib/features/order/data/repository/order_repository.dart
+++ lib/features/order/data/repository/order_repository.dart
@@ -1,5 +1,7 @@
 import 'package:al_omda/core/api/api_constances.dart';
 import 'package:al_omda/core/api/api_methods.dart';
+import 'package:al_omda/core/error/exception.dart';
+import 'package:al_omda/features/order/data/models/order_model.dart';
 import 'package:al_omda/features/order/domain/repository/base_order_repository.dart';
 import 'package:dartz/dartz.dart';
 
@@ -9,27 +11,42 @@ class OrderRepository extends BaseOrderRepository {
   OrderRepository({required this.api});
 
   @override
-  Future<Either<String, bool>> makeOrder({
-    required String date,
-    required String time,
-    required int paymentId,
-    required int addressId,
-  }) async {
-    final response = await api.post(
-      ApiConstances.makeOrder,
-      data: {
-        'shipping_day': date,
-        'shipping_time': time,
-        'payment_id': paymentId,
-        'address_id': addressId,
-      },
-    );
-    return response;
-  }
-
+  // Future<Either<String, bool>> makeOrder({
+  //   required String date,
+  //   required String time,
+  //   required int paymentId,
+  //   required int addressId,
+  // }) async {
+  //   final response = await api.post(
+  //     ApiConstances.makeOrder,
+  //     data: {
+  //       'shipping_day': date,
+  //       'shipping_time': time,
+  //       'payment_id': paymentId,
+  //       'address_id': addressId,
+  //     },
+  //   );
+  //   return response;
+  // }
   //   Future<void> clearCartAfterOrder() async {
   //   // Call cart repository to clear items
   //   await _cartRepository.clearCart();
   // }
   // }}
+  @override
+  Future<Either<String, OrderResponseModel>> makeOrder(
+    OrderRequestModel request,
+  ) async {
+    try {
+      final response = await api.post(
+        ApiConstances.makeOrder,
+        data: request.toJson(),
+      );
+
+      final orderResponse = OrderResponseModel.fromJson(response.data);
+      return Right(orderResponse);
+    } on ServerException catch (e) {
+      return Left(e.errorModel.errorMessage);
+    }
+  }
 }
--- lib/features/order/domain/repository/base_order_repository.dart
+++ lib/features/order/domain/repository/base_order_repository.dart
@@ -1,10 +1,12 @@
+import 'package:al_omda/features/order/data/models/order_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class BaseOrderRepository {
-  Future<Either<String, bool>> makeOrder({
-    required String date,
-    required String time,
-    required int paymentId,
-    required int addressId,
-  });
+  // Future<Either<String, bool>> makeOrder({
+  //   required String date,
+  //   required String time,
+  //   required int paymentId,
+  //   required int addressId,
+  // });
+   Future<Either<String, OrderResponseModel>> makeOrder(OrderRequestModel request);
 }
